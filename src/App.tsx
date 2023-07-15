6edbd7b687392791908d74b372447f02ea3a424a
Omar Saleem <osaleem@usc.edu>
2023-07-15 21:32:23 +0700

    Added necessary routing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import Form from "./components/Form";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<>
-			<Form />
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Form />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,7 +12,7 @@ import {
 } from "@chakra-ui/react";
 import { MdAccountCircle } from "react-icons/Md";
 import { AiFillLock } from "react-icons/Ai";
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 import clouds from "../assets/clouds.jpg";
 
 const Form = () => {
@@ -25,57 +25,77 @@ const Form = () => {
     textAlign: "center",
   };
 
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const loginInfo = { email: "", password: "" };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (emailRef.current !== null) loginInfo.email = emailRef.current.value;
+    if (passwordRef.current !== null)
+      loginInfo.password = passwordRef.current.value;
+    console.log(loginInfo);
+  };
+
   return (
-    <Box
-      height="100vh"
-      sx={boxStyles}
-      bgImage={clouds}
-      backgroundPosition="center"
-      // backgroundRepeat="no-repeat"
-    >
-      <Card
-        maxW="lg"
-        borderWidth={2}
-        borderRadius="lg"
-        overflow="hidden"
-        p="50"
-        bgColor="#002244"
+    <form onSubmit={handleSubmit}>
+      <Box
+        height="100vh"
+        sx={boxStyles}
+        bgImage={clouds}
+        backgroundPosition="center"
       >
-        <CardHeader>
-          <Heading fontSize="2em" marginY={4}>
-            Sign in
-          </Heading>
-        </CardHeader>
-        <Stack>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <MdAccountCircle color="gray.300" />
-            </InputLeftElement>
-            <Input width={340} placeholder="Email" />
-          </InputGroup>
+        <Card
+          maxW="lg"
+          borderWidth={2}
+          borderRadius="lg"
+          overflow="hidden"
+          p="50"
+          bgColor="#002244"
+        >
+          <CardHeader>
+            <Heading fontSize="2em" marginY={4}>
+              Sign in
+            </Heading>
+          </CardHeader>
+          <Stack>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <MdAccountCircle color="gray.300" />
+              </InputLeftElement>
+              <Input width={340} placeholder="Email" ref={emailRef} />
+            </InputGroup>
 
-          <InputGroup>
-            <InputLeftElement>
-              <AiFillLock color="gray.300" />
-            </InputLeftElement>
-            <Input
-              width={340}
-              placeholder="Password"
-              type={show ? "text" : "password"}
-            />
-          </InputGroup>
-        </Stack>
+            <InputGroup>
+              <InputLeftElement>
+                <AiFillLock color="gray.300" />
+              </InputLeftElement>
+              <Input
+                width={340}
+                placeholder="Password"
+                type={show ? "text" : "password"}
+                ref={passwordRef}
+              />
+            </InputGroup>
+          </Stack>
 
-        <HStack marginX={20}>
-          <Button colorScheme="purple" marginY={4}>
-            Sign up
-          </Button>
-          <Button colorScheme="purple" className="btn btn-primary" marginY={4}>
-            Log in
-          </Button>
-        </HStack>
-      </Card>
-    </Box>
+          <HStack marginX={20}>
+            <Button colorScheme="purple" marginY={4}>
+              Sign up
+            </Button>
+            <Button
+              colorScheme="purple"
+              className="btn btn-primary"
+              marginY={4}
+              type="submit"
+            >
+              Log in
+            </Button>
+          </HStack>
+        </Card>
+      </Box>
+    </form>
   );
 };
 
