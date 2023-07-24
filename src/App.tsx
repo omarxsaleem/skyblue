8c4a59dcb3d50776560f29c8d433e360aa192adf
Omar Saleem <osaleem@usc.edu>
2023-07-24 13:21:02 +0700

    Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Form from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Form />} />
+        <Route path="/login" element={<Form />}></Route>
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="#002244">
+          <NavBar />
+        </GridItem>
+        <Show above="lg"></Show>
+      </Grid>
     </>
   );
 }
