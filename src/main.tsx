0e75ad7b3684550d0e6e56b6efb5e9b7dea3565a
Omar Saleem <osaleem@usc.edu>
2023-08-01 08:11:28 +1000

    Cleaned up router stuff

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,22 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 import {
+<<<<<<< HEAD
 	BrowserRouter,
 	RouterProvider,
 	createBrowserRouter,
+=======
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+>>>>>>> Cleaned up router stuff
 } from "react-router-dom";
 import router from "./routing/routes";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+<<<<<<< HEAD
 	<React.StrictMode>
 		<ChakraProvider>
 			<BrowserRouter>
@@ -25,4 +32,11 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 			</BrowserRouter>
 		</ChakraProvider>
 	</React.StrictMode>
+=======
+  <React.StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+>>>>>>> Cleaned up router stuff
 );
