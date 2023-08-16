0d2b8c02386010c571fc587bbe2f2d897fdd3e90
Omar Saleem <osaleem@usc.edu>
2023-08-16 01:36:28 -0500

    Fixed login

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { Routes, Route } from "react-router";
-import { Form } from "react-router-dom";
 import Signup from "./components/Signup";
 import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 import Home from "./components/Flights";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/login" element={<Form />}></Route>
-				<Route path="/signup" element={<Signup />} />
-			</Routes>
-			<Grid
-				templateAreas={{
-					base: `"nav" "main"`,
-					lg: `"nav nav" "aside main"`,
-				}}
-			>
-				<GridItem area="nav" bg="#002244">
-					<NavBar />
-				</GridItem>
-				<Show above="lg"></Show>
-				<Home />
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="#002244">
+          <NavBar />
+        </GridItem>
+        <Show above="lg"></Show>
+        <Home />
+      </Grid>
+    </>
+  );
 }
 
 export default App;
