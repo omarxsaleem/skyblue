249cec0aa69f44cf393b89ad5ccac6dc38199b40
omarn33 <omarnaeem333@gmail.com>
2023-08-16 00:54:13 -0500

    Revert "Update README.md"
    This reverts commit ab21025dcf5169d00b4d1b9ede82044781e535d2.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import Home from "./components/Flights";
 import { Routes, Route } from "react-router";
 import { Form } from "react-router-dom";
 import Signup from "./components/Signup";
+import NavBar from "./components/NavBar";
+import Home from "./components/Flights";
 
 function App() {
 	return (
